e8418e0547827d7493bb3f3ee43445d0f02fe71a
Brad Holland <hollanbm@gmail.com>
2020-02-03 20:26:47 -0500

    initial checkin

---
+++ FilebotSupportBot.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env.CLIENT_TOKEN);
+
