0670cf19d321f7e8182f7f564a071dcad9b0f726
Brad Holland <hollanbm@gmail.com>
2020-02-05 22:14:52 -0500

    add noemail command

--- FilebotSupportBot.js
+++ FilebotSupportBot.js
@@ -17,6 +17,20 @@ client.on('message', msg =>
 	{
 		msg.channel.send('How to request help: https://www.filebot.net/forums/viewtopic.php?f=5&t=1868')
 	}
+	else if(msg.content === prefix + 'noemail')
+	{
+		msg.channel.send(
+			'After purchasing FileBot via Paddle, you will receive 2 emails. \
+			The first email from help@paddle.com will contain your license as plain text. \
+			The second email from license@filebot.net will contain your license as file attachment.\
+			\n\n \
+			If you did not receive 2 emails, please check your Spam folder. \
+			\n\n \
+			If you have received no email at all then you have likely mistyped your email address or your mail server is rejecting emails. \
+			In this case, please PM @rednoah your name and @gmail.com address, and ideally your order number \
+			(e.g. P1234567) if possible, so @rednoah can manually resend your license.\
+			')
+	}
 });
 
 client.login(process.env.CLIENT_TOKEN);
