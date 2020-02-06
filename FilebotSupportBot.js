1c1c630ad7d19a4386cae52f044a26f4c0f4b6b0
Brad Holland <hollanbm@gmail.com>
2020-02-05 21:59:33 -0500

    add help command

--- FilebotSupportBot.js
+++ FilebotSupportBot.js
@@ -13,6 +13,10 @@ client.on('message', msg =>
 	if (msg.content === prefix + 'license') {
 		msg.channel.send('For help activating, recovering, or installing your license: https://www.filebot.net/forums/viewtopic.php?f=8&t=6121');
 	}
+	else if(msg.content === prefix + 'help')
+	{
+		msg.channel.send('How to request help: https://www.filebot.net/forums/viewtopic.php?f=5&t=1868')
+	}
 });
 
 client.login(process.env.CLIENT_TOKEN);
