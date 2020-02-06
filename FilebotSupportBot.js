5254ae2ec405a2e672f04d59709edeae62266b9e
Brad Holland <hollanbm@gmail.com>
2020-02-05 21:46:05 -0500

    prefix env var, add license command

--- FilebotSupportBot.js
+++ FilebotSupportBot.js
@@ -1,15 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+prefix = process.env.PREFIX
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 
-client.on('message', msg => {
-  if (msg.content === '!ping') {
-    msg.channel.send('Pong2!');
-  }
+client.on('message', msg => 
+{
+	if (msg.content === prefix + 'license') {
+		msg.channel.send('For help activating, recovering, or installing your license: https://www.filebot.net/forums/viewtopic.php?f=8&t=6121');
+	}
 });
 
 client.login(process.env.CLIENT_TOKEN);
