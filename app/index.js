400a4330ba101967e53607be246057538a43224c
Brad Holland <bholland@bholland-rmac2.bholland.lan>
2020-09-23 16:50:44 -0400

    Add fuzzy matching on invalid command input

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const stringSimilarity = require('string-similarity');
 
-prefix = process.env.PREFIX
+var prefix = process.env.PREFIX
+var fuzzyMatchCutoff = process.env.FUZZY_MATCH_CUTOFF
+
 
 var fs = require('fs');
 //this is synchronous, if list gets too large, switch to async
 var commands = JSON.parse(fs.readFileSync('app/commands.json', 'utf8'));
+var validCommandsText = `List of valid commands:\n${commands.map(item => "`" + prefix+item.cmd).sort().join("` ") + "`"}`
+
 
 client.once('ready', () => 
 {
@@ -21,7 +26,19 @@ client.on('message', message =>
 	}
 	else if(message.isMentioned(client.user))
 	{
-		message.channel.send("List of valid commands:\n" + commands.map(item => "`" + prefix+item.cmd).sort().join("` ") + "`");
+		message.channel.send(validCommandsText);
+	}
+	else if(message.content.charAt(0) === prefix)
+	{
+		var similarity = stringSimilarity.findBestMatch(message.content.substring(1), commands.map(item => item.cmd));
+		if(similarity.bestMatch.rating >= fuzzyMatchCutoff)
+		{
+			message.channel.send(`ERROR: Command not found.\n\nPossible match: \`${prefix}${similarity.bestMatch.target}\``);
+		}
+		else
+		{
+			message.channel.send(`ERROR: Command not found.\n\n${validCommandsText}`);
+		}
 	}
 });
 
